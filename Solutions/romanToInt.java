a1b198d6a4a01263af7d8e1fa2812f172eb783c6
YSiva99 <76894653+YSiva99@users.noreply.github.com>
2025-02-20 21:35:59 +0530

    romanToInteger Converter solution
    romanToInteger Converter solution

---
+++ Solutions/romanToInt.java
@@ -0,0 +1,37 @@
+package Solutions;
+import java.util.*;
+
+public class romanToInt {
+    public static  int romanToInt(String s){
+    Map<Character,Integer> romanMap=new HashMap<>();
+    romanMap.put('I',1);
+    romanMap.put('V',5);
+    romanMap.put('X',10);
+    romanMap.put('L',50);
+    romanMap.put('C',100);
+    romanMap.put('D',500);
+    romanMap.put('M',1000);
+
+    int result=0;
+    int prevValue=0;
+
+    for(int i=s.length()-1;i>=0;i--){
+        int currentValue=romanMap.get(s.charAt(i));
+
+        if(currentValue<prevValue){
+            result-=currentValue;
+        }
+        else{
+            result+=currentValue;
+        }
+        prevValue=currentValue;
+    }
+    return result;
+
+    }
+    public static void main(String[] args){
+        System.out.println("The romanToIntege value is:"+romanToInt("LVIII"));
+        System.out.println("The romanToIntege value is:"+romanToInt("MCMXCIV"));
+    }
+
+}
